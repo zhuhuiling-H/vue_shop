0a2257d450a26cac187672384c8daf8f54be6f44
zhuhuiling-H <18508471805@163.com>
2019-11-05 16:18:13 +0800

    init home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,14 @@
 <template>
-    <div>
-        this is home
-        <el-button type="info" @click="logout"> 退出 </el-button>
-    </div>
+      <el-container class="home-container">
+          <el-header>
+            Header
+           <el-button type="info" @click="logout"> 退出 </el-button>
+          </el-header>
+          <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-main>Main</el-main>
+          </el-container>
+      </el-container>
 </template>
 
 <script>
@@ -18,4 +24,16 @@ export default {
 </script>
 
 <style lang='less' scoped>
+.home-container {
+  height: 100%;
+}
+.el-header{
+  background-color:#373D41;
+}
+.el-aside{
+  background-color:#333744;
+}
+.el-main{
+  background-color:#eaedf1;
+}
 </style>
